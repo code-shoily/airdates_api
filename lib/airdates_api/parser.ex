143f7b102e3de387f9419e1ab26a04bea1ff972b
mafinar <mafinar@gmail.com>
2018-07-13 22:14:28 -0400

    Add custom type for show_line

--- lib/airdates_api/parser.ex
+++ lib/airdates_api/parser.ex
@@ -1,8 +1,10 @@
 defmodule AirdatesApi.Parser do
+  @type show_line :: [id: binary(), date: binary(), title: binary()]
+
   @doc """
   Fetch data from website and parses it.
   """
-  @spec parse() :: [{binary(), binary(), binary()}]
+  @spec parse() :: [show_line]
   def parse do
     AirdatesApi.Client.get_airdates()
     |> Floki.parse()
@@ -10,11 +12,7 @@ defmodule AirdatesApi.Parser do
     |> Enum.flat_map(&parse_line/1)
   end
 
-  @spec parse_line(Floki.HTMLTree.t()) :: [
-          id: binary(),
-          date: binary(),
-          title: binary()
-        ]
+  @spec parse_line(Floki.HTMLTree.t()) :: [show_line()]
   defp parse_line({_, [_, {"data-date", date}], data}) do
     data
     |> Enum.map(fn
@@ -27,7 +25,7 @@ defmodule AirdatesApi.Parser do
     |> Enum.filter(& &1)
   end
 
-  @spec dispatch(binary(), binary(), binary()) :: [id: binary(), date: binary(), title: binary()]
+  @spec dispatch(binary(), binary(), binary()) :: show_line
   defp dispatch(date, id, title) do
     # # TODO Send this to the GenServer Store
     [id: id, date: date, title: title]

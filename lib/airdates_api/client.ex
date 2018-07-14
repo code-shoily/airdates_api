cfd6e10094dc6f3bec492306f3e0274d145293e2
mafinar <mafinar@gmail.com>
2018-07-13 21:03:16 -0400

    Remove Fetcher Module

--- lib/airdates_api/client.ex
+++ lib/airdates_api/client.ex
@@ -4,6 +4,9 @@ defmodule AirdatesApi.Client do
   plug(Tesla.Middleware.BaseUrl, "http://www.airdates.tv")
   plug(Tesla.Middleware.Compression, format: "gzip")
 
+  @doc """
+  Gets all data from the URL
+  """
   def get_airdates do
     case get("/") do
       {:ok, %Tesla.Env{body: body}} -> body

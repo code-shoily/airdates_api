32891f693bcb118aa1b9611692c11d64ef36d527
mafinar <mafinar@gmail.com>
2018-07-15 01:59:16 -0400

    Add descriptions of fields

--- lib/airdates_api/web/schema.ex
+++ lib/airdates_api/web/schema.ex
@@ -5,20 +5,29 @@ defmodule AirdatesApi.Web.Schema do
   import_types(AirdatesApi.Web.Types)
 
   query do
-    @desc "Lists all shows"
+    @desc """
+    Lists all shows. Can be filtered by id, seriesId, title, or date.
+    Any of the three criteria can be filtered on. Episode can only be used with title
+    """
     field :shows, list_of(:show) do
-      arg(:id, :id)
-      arg(:series_id, :id)
-      arg(:title, :string)
-      arg(:date, :string)
-      arg(:episode, :string)
-      arg(:sort_by, :string)
+      arg(:id, :id, description: "The ID for the show airred on a particular date")
+      arg(:series_id, :id, description: "The ID given by airdates.tv of the show")
+
+      arg(
+        :title,
+        :string,
+        description: "The title of the show, can be used together with 'episode'"
+      )
+
+      arg(:date, :string, description: "The date of the show, in YYYYMMDD format")
+      arg(:episode, :string, description: "Episode in SXXEXX format")
+      arg(:sort_by, :string, description: "Either 'title' or 'date', sort field for show list")
       resolve(&Resolvers.series/3)
     end
   end
 
   mutation do
-    @desc "Fetches all shows from http://airdates.tv"
+    @desc "(Re-)fetches all shows from http://airdates.tv"
     field :reload, list_of(:show) do
       resolve(&Resolvers.reload/3)
     end

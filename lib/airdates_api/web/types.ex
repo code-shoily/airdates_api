e2f6d2cc3fce97674c8ccff5ffa40c4327ac4f0e
mafinar <mafinar@gmail.com>
2018-07-15 01:21:25 -0400

    Show type

---
+++ lib/airdates_api/web/types.ex
@@ -0,0 +1,11 @@
+defmodule AirdatesApi.Web.Types do
+  use Absinthe.Schema.Notation
+
+  object :show do
+    field(:id, :id)
+    field(:date, :string)
+    field(:series_id, :string)
+    field(:description, :string)
+    field(:title, :string)
+  end
+end
